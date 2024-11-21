2656638dad273d9d1b823b982068352d0a26afbd
mbougar303 <mbougar303@g.educaand.es>
2024-11-21 23:39:28 +0100

    Cambios chats

--- app/src/main/java/com/mbougar/clonewhatsap_pmdm/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/mbougar/clonewhatsap_pmdm/ui/screens/ChatScreen.kt
@@ -1,6 +1,7 @@
 package com.mbougar.clonewhatsap_pmdm.ui.screens
 
 import android.annotation.SuppressLint
+import android.provider.CalendarContract.Colors
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,24 +20,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.DoneAll
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.outlined.AttachFile
+import androidx.compose.material.icons.outlined.CameraAlt
+import androidx.compose.material.icons.outlined.EmojiEmotions
+import androidx.compose.material.icons.outlined.Mic
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material.icons.outlined.Videocam
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -47,16 +63,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.mbougar.clonewhatsap_pmdm.contactMessagesMap
 import com.mbougar.clonewhatsap_pmdm.model.Contact
 import com.mbougar.clonewhatsap_pmdm.model.Message
+import com.mbougar.clonewhatsap_pmdm.ui.theme.blackBackground
 import com.mbougar.clonewhatsap_pmdm.ui.theme.messageBlack
 import com.mbougar.clonewhatsap_pmdm.ui.theme.messageGreen
 import com.mbougar.clonewhatsap_pmdm.ui.theme.readMessageColor
@@ -84,8 +103,8 @@ fun ChatScreen(navController: NavHostController, innerPadding: PaddingValues, co
             topBar = {
                 HeaderChat(contact) { navController.navigate("main") }
             },
-            bottomBar = { ChatInput { string ->
-                messages.add(Message(
+            bottomBar = { ChatInput2 { string ->
+                contactMessagesMap[contact.nombre]?.add(Message(
                     id = messages.size + 1,
                     nombreUsuario = "Me",
                     textoMensaje = string,
@@ -248,7 +267,113 @@ fun ChatInput(onSend: (String) -> Unit) {
                 text = ""
             }
         }) {
-            Icon(Icons.Default.Send, contentDescription = "Enviar", tint = Color(0xFF128C7E))
+            if (text != "" ) {
+                Icon(Icons.Default.Send, contentDescription = "Enviar", tint = Color(0xFF128C7E))
+            } else {
+                Icon(Icons.Default.Mic, contentDescription = "Micro", tint = Color(0xFF128C7E))
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatInput2(onSend: (String) -> Unit) {
+    var text by remember { mutableStateOf("") }
+
+    Row(
+        modifier = Modifier
+            .height(72.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val shape = MaterialTheme.shapes.small.copy(CornerSize(50.dp))
+
+        TextField(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .weight(1f),
+            value = text,
+            onValueChange = { newText -> text = newText },
+            shape = shape,
+            leadingIcon = {
+                IconButton(
+                    onClick = { },
+                    colors = IconButtonDefaults.iconButtonColors(
+                        contentColor = Color.Gray,
+                        containerColor = Color.Transparent
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.EmojiEmotions,
+                        contentDescription = "Emoji"
+                    )
+                }
+            },
+            trailingIcon = {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(end = 8.dp)
+                ) {
+                    IconButton(
+                        onClick = { },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = Color.Gray,
+                            containerColor = Color.Transparent
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.AttachFile,
+                            contentDescription = "Attach File"
+                        )
+                    }
+                    if (text.isBlank()) {
+                        IconButton(
+                            onClick = { },
+                            modifier = Modifier.size(28.dp),
+                            colors = IconButtonDefaults.iconButtonColors(
+                                contentColor = Color.Gray,
+                                containerColor = Color.Transparent
+                            )
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.CameraAlt,
+                                contentDescription = "Camera"
+                            )
+                        }
+                    }
+                }
+            },
+            placeholder = { Text("Type a message", color = Color.Gray) },
+            textStyle = TextStyle(color = Color.White),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color(0xFF262D31),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+
+        FloatingActionButton(
+            onClick = {
+                if (text.isNotBlank()) {
+                    onSend(text)
+                    text = ""
+                }
+            },
+            modifier = Modifier
+                .padding(end = 16.dp)
+                .wrapContentSize(),
+            elevation = FloatingActionButtonDefaults.elevation(8.dp),
+            containerColor = messageGreen,
+            contentColor = Color.White,
+            shape = CircleShape
+        ) {
+            Icon(
+                imageVector = if (text.isNotBlank()) Icons.Outlined.Send else Icons.Outlined.Mic,
+                contentDescription = if (text.isNotBlank()) "Send Message" else "Record Audio"
+            )
         }
     }
 }
